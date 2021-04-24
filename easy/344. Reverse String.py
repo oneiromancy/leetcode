02dad1ceaf20869cd40ca82a7f35e6f8ab283447
Henri <oneiromancy.ai@gmail.com>
2021-04-24 15:47:57 +0100

    problem 344

---
+++ easy/344. Reverse String.py
@@ -0,0 +1,38 @@
+def reverseString(s):
+    """
+    Do not return anything, modify s in-place instead.
+    """
+    size = len(s)
+
+    for idx in range(size // 2):
+        s[idx], s[size - idx - 1] = s[size - idx - 1], s[idx]
+
+    return s
+
+print(reverseString(["h","e","l","l","o"]))
+        
+
+# Write a function that reverses a string. The input string is given as an array of characters s.
+
+ 
+
+# Example 1:
+
+# Input: s = ["h","e","l","l","o"]
+# Output: ["o","l","l","e","h"]
+
+# Example 2:
+
+# Input: s = ["H","a","n","n","a","h"]
+# Output: ["h","a","n","n","a","H"]
+
+ 
+
+# Constraints:
+
+#     1 <= s.length <= 105
+#     s[i] is a printable ascii character.
+
+ 
+
+# Follow up: Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
