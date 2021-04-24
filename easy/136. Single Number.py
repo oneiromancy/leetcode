1224754e02133de7c85fa7d13fd680c3bf8d9ce1
Henri <oneiromancy.ai@gmail.com>
2021-04-24 21:03:55 +0100

    problem 136

---
+++ easy/136. Single Number.py
@@ -0,0 +1,41 @@
+def singleNumber(nums):
+    d = {}
+
+    for num in nums:
+        d[num] = d[num] + 1 if num in d else 1
+
+    for k, v in d.items():
+        if v == 1:
+            return k
+
+print(singleNumber([4,1,2,1,2]))
+
+# Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+# Follow up: Could you implement a solution with a linear runtime complexity and without using extra memory?
+
+ 
+
+# Example 1:
+
+# Input: nums = [2,2,1]
+# Output: 1
+
+# Example 2:
+
+# Input: nums = [4,1,2,1,2]
+# Output: 4
+
+# Example 3:
+
+# Input: nums = [1]
+# Output: 1
+
+ 
+
+# Constraints:
+
+#     1 <= nums.length <= 3 * 104
+#     -3 * 104 <= nums[i] <= 3 * 104
+#     Each element in the array appears twice except for one element which appears only once.
+
