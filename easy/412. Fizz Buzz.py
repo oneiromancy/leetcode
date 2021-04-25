1e3f04d462923b5773973a0e2e240ca48f3a335c
Henri <oneiromancy.ai@gmail.com>
2021-04-25 13:44:27 +0100

    problem 412

---
+++ easy/412. Fizz Buzz.py
@@ -0,0 +1,48 @@
+def fizzBuzz(n):
+    fb = []
+
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            fb.append("FizzBuzz")
+        elif i % 3 == 0:
+            fb.append("Fizz")
+        elif i % 5 == 0:
+            fb.append("Buzz")
+        else:
+            fb.append(str(i))
+
+    return fb
+
+print(fizzBuzz(15))
+        
+
+# Given an integer n, return a string array answer (1-indexed) where:
+
+#     answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+#     answer[i] == "Fizz" if i is divisible by 3.
+#     answer[i] == "Buzz" if i is divisible by 5.
+#     answer[i] == i if non of the above conditions are true.
+
+ 
+
+# Example 1:
+
+# Input: n = 3
+# Output: ["1","2","Fizz"]
+
+# Example 2:
+
+# Input: n = 5
+# Output: ["1","2","Fizz","4","Buzz"]
+
+# Example 3:
+
+# Input: n = 15
+# Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+ 
+
+# Constraints:
+
+#     1 <= n <= 104
+
