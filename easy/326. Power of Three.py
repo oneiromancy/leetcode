c642c4f72f9c9c236e585e7a86d5505385723d4c
Henri <oneiromancy.ai@gmail.com>
2021-05-03 22:06:52 +0100

    problem 326

---
+++ easy/326. Power of Three.py
@@ -0,0 +1,13 @@
+def isPowerOfThree(n):
+    if n == 0:
+        return False
+
+    while n % 3 == 0:
+        n = n / 3
+
+    if n == 1:
+        return True
+    else:
+        return False
+
+print(isPowerOfThree(1))
